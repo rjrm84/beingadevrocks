7235375b77f699e1633d73bc6165eb075b26fca8
Raul Rodriguez <rjrm84@gmail.com>
2019-01-15 18:22:47 -0300

    listo para produccion

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MainComponent } from './pages/main/main.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
